7c5bfef59bb9f0edf8066fa5de7e9339da296b41
turek15 <52458972+turek15@users.noreply.github.com>
2023-01-21 11:56:41 -0500

    Fixes to main.py

--- src/pi/main.py
+++ src/pi/main.py
@@ -1,45 +1,74 @@
 import RPi.GPIO as io
 import time
-io.setwarnings(False)
-io.setmode(io.BCM)
+import json
 
+
+
+#represents the GPIO pins for each respective pump
+
+PUMP_TIME_CONSTANT=10
+ALCOHOL_TIME_CONSTANT=5
 
 class cocktailcreate():
     def __init__(self):
+        #initialize GPIO 
+        io.setwarnings(False)
+        io.setmode(io.BCM)
+
+        self.senseconfiguration = False #assume that it is not ready to receive a drink 
         #this will be the initial state of the machine
         #load pump configuration from a file for each pump
         self.pumpconfiguration=cocktailcreate.pumpconfig()
 
+        #setting up each pin to the appropriate pump 
+        for i in range(0,len(self.pumpconfiguration)):
+            io.setup(self.pumpconfiguration[i]['Pin'], io.OUT) 
+        
 
-    def pourdrink(self):
+    def pourdrink(self, ing1, ing2=None, ing3=None):
 
-        #check to see if cup is there using sensor functions
-        #call two pumps to pour corresponding ingredients, one at a time
-        #
-        if sense:
-            self.pump(ingredient_one)
-            self.pump(ingredient_two)
-            self.rotate()
+
+        #this function is going to be called by the web app upon the ordering of a drink
+        if self.senseconfiguration:
+            self.pumprun(ing1,ing2)
         else:
+            print("Fail")
             #alert web app that cup is not present 
 
 
-    def pump(self):
+    #def pump(self):
     
     @staticmethod
-    def pumpconfig(self):
+    def pumpconfig():
+        return json.load(open('drinks.json'))
+
+        #this function returns a json list of pump configurations with it's appropriate drinks
+
 
     @staticmethod
-    def senseconfig(self):
+    def senseconfig():
         #load sensor status into this function
+        return self.senseconfiguration =  #output of ultra.py function  
 
-    def turntable(self):
+    #def turntable(self):
+
+    def pumprun(self):
+        if (self.pumpconfiguration == 2)
+            io.output(pump, io.HIGH)
+            time.sleep(POP_TIME_CONSTANT)
+            io.output(pump, io.LOW)
+        else:
+            io.output(pump, io.HIGH)
+            time.sleep(AlCOHOL_TIME_CONSTANT)
+            io.output(pump, io.LOW)        
 
     def run(self):
         try :
             while True:
+                print("hello")
         except:
-            dsjfsf
+            print("false")
 
+bartender = cocktailcreate()
 
-
+# this is what will be called by the web app bartender.pourdrink(arg1, arg2), create a single object called "bartender" then use that object to access the methods of the class
