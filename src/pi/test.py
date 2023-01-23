b317276566d139555d3ee11da649d3ac8aa9f034
turek15 <52458972+turek15@users.noreply.github.com>
2023-01-23 00:24:56 -0500

    small changes

--- src/pi/test.py
+++ src/pi/test.py
@@ -1,9 +1,17 @@
 import json
 
-pumpconfiguration=json.load(open('drinks.json'))
+
 
 #print(pumpconfiguration['OJ'])
 
+
+
+
+
+def pumpconfig():
+    return json.load(open('drinks.json'))
+
+pumpconfiguration=pumpconfig()
 
 for i in pumpconfiguration:
     print(pumpconfiguration[i][2]['Pin'])
