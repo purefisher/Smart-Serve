0ef8e5bf4251c886baeeb81e6d4e371f8bff610d
turek15 <52458972+turek15@users.noreply.github.com>
2023-03-26 00:46:11 -0400

    fixed volume to cocktail file

--- src/backend/server/cocktail.py
+++ src/backend/server/cocktail.py
@@ -247,6 +247,47 @@ class cocktailcreate():
         for threads in pump_threads:
             threads.join()
 
+    def Volume(self, ing1, ing2=None, ing3=None, ing4=None, ing5=None):
+        result = [[0,0],[0,0],[0,0],[0,0],[0,0]]
+        if ing2 == None:
+            result[0] = [x.strip() for x in ing1.split(',')]
+        elif ing3 == None:
+            result[0] = [x.strip() for x in ing1.split(',')]
+            result[1] = [x.strip() for x in ing2.split(',')]
+        elif ing4 == None:
+            result[0] = [x.strip() for x in ing1.split(',')]
+            result[1] = [x.strip() for x in ing2.split(',')]
+            result[2] = [x.strip() for x in ing3.split(',')]
+        elif ing5 == None:
+            result[0] = [x.strip() for x in ing1.split(',')]
+            result[1] = [x.strip() for x in ing2.split(',')]
+            result[2] = [x.strip() for x in ing3.split(',')]
+            result[3] = [x.strip() for x in ing4.split(',')]
+        else:
+            result[0] = [x.strip() for x in ing1.split(',')]
+            result[1] = [x.strip() for x in ing2.split(',')]
+            result[2] = [x.strip() for x in ing3.split(',')]
+            result[3] = [x.strip() for x in ing4.split(',')]
+            result[4] = [x.strip() for x in ing5.split(',')]
+            
+        numShots = 0
+        numPops = 0
+        popPourTime = 0
+        i = 0
+        while i < 5:
+            if(result[i][0] == 0):
+                break
+            elif result[i][1] == '1' :
+                numShots +=1
+            elif(result[i][1] == '2'):
+                numPops +=1
+            i+=1
 
+        popRemaining = 4 - numShots
+        if(numPops > 0):
+            popPourTime = popRemaining / numPops
+
+        return popPourTime
+        
 
 
