bb52f48489b0844d553f275e0f4db41ffab240a2
turek15 <turekm@mcmaster.ca>
2023-03-31 23:33:15 -0400

    Liquid level sensor now works accurately for our drink volume

--- src/backend/server/constants.py
+++ src/backend/server/constants.py
@@ -5,7 +5,7 @@ cleaning_constant = 10
 cup_distance_limit_lower = 4.6
 cup_distance_limit_upper = 8.0
 cup_distance_limit_turn = 3.6
-cup_liquid_limit = 10 #this goes up to the lines on a solo cup
+cup_liquid_limit = 13 #this goes up to the lines on a solo cup was 10
 
 pump_one_pin = 15
 pump_two_pin = 13
